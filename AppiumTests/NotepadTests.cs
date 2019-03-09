719bcab902596a4d4d3b5ead2fef0ef5c07b7e25
Adriano Vega <adrianovega@hotmail.com>
2019-03-09 11:59:40 +0100

    Added example test InputTextInNotepadWindow

---
+++ AppiumTests/NotepadTests.cs
@@ -0,0 +1,64 @@
+﻿namespace AppiumTests
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium.Appium;
+    using OpenQA.Selenium.Appium.Windows;
+
+    /// <summary>
+    /// Class for implementing tests on the Notepad windows application.
+    /// </summary>
+    [TestClass]
+    public class NotepadTests
+    {
+        private const string testFolderRelativePath = @"../MyTestFolder/";
+        private const string testFileName = "MyTestFile.txt";
+
+        private WindowsDriver<WindowsElement> windowsDriver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            DirectoryInfo testFolder = GetTestDirectory();
+
+            // Create the text file or overwrite it if it exists.
+            string testFileFullPath = $"{testFolder.FullName}/{testFileName}";
+            File.WriteAllText(testFileFullPath, "");
+
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", @"C:\Windows\System32\notepad.exe");
+            appiumOptions.AddAdditionalCapability("platformName", "Windows");
+            appiumOptions.AddAdditionalCapability("deviceName", "WindowsPC");
+            appiumOptions.AddAdditionalCapability("appArguments", testFileFullPath);
+
+            this.windowsDriver = new WindowsDriver<WindowsElement>(new Uri(@"http://127.0.0.1:4723/wd/hub"), appiumOptions);
+        }
+
+        [TestMethod]
+        public void InputTextInNotepadWindow()
+        {
+            // Arrange.
+            string expectedText = "Say hello world.";
+
+            // Act.
+            WindowsElement editTextBox = this.windowsDriver.FindElementByClassName("Edit");
+            editTextBox.SendKeys(expectedText);
+
+            // Assert.
+            Assert.AreEqual(expectedText, editTextBox.Text, $"The expected text was '{expectedText}' but found '{editTextBox.Text}'.");
+        }
+
+        private static DirectoryInfo GetTestDirectory()
+        {
+            DirectoryInfo testFolder = new DirectoryInfo(testFolderRelativePath);
+
+            if (!testFolder.Exists)
+            {
+                testFolder.Create();
+            }
+
+            return testFolder;
+        }
+    }
+}
---
+++ AppiumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppiumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppiumTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1acd3289-6982-45b7-9bd5-5903fb413de2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
